c94da2c184c1f588299111b74d3d14155babf501
lemisu <369744853@qq.com>
2020-10-04 05:40:56 +0800

    grid

--- src/api/gridAPI.ts
+++ src/api/gridAPI.ts
@@ -1,34 +1,16 @@
 /**
- * 获取网格行初始值
+ * 获取网格行初始值1,2,2,3,4,4,5,6,6,7,8,8...
  */
 export function getGridRowStart(index : number) {
-    let gridRowStart : number = 0;
-    for (let i = 0; i <= index; i++) {
-        if (i % 3 !== 2) {
-            ++gridRowStart
-        }
-    }
-    return gridRowStart
+    let baseNum = 2 * (index / 3) + 1
+    let ceilNum = Math.ceil(baseNum)
+    let floorNum = Math.floor(baseNum)
+    return [baseNum, ceilNum, floorNum][index % 3];
 }
 
 /**
- * 获取网格列初始值
+ * 获取网格列初始值2,1,3,2,1,3,2,1,3...
  */
 export function getGridColumnStart(index : number) {
-    let gridColumnStart : number = 0;
-    switch(index % 3) {
-        case 0:
-            gridColumnStart = 2;
-            break;
-        case 1:
-            gridColumnStart = 1;
-            break;
-        case 2:
-            gridColumnStart = 3;
-            break;
-        default:
-            gridColumnStart = 2;
-            break;
-    }
-    return gridColumnStart;
+    return [2, 1, 3][index % 3];
 }
